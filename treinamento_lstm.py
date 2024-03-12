65d1fff008897443c2abf862dece4fa91f87fbc7
Nairon Nunes <snairon@stj.jus.br>
2024-03-12 08:58:46 -0300

    Limpando comentarios do treinamento lstm

--- treinamento_lstm.py
+++ treinamento_lstm.py
@@ -1,11 +1,7 @@
-import sys
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
-from sklearn.pipeline import make_pipeline
-from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from joblib import dump, load
 from tensorflow.keras.layers import Embedding, LSTM, Dense
@@ -41,53 +37,12 @@ print('\nExplorando os rótulos de classificação')
 print(df['COD_ASSUNTO_CLASSIFICACAO'].value_counts())
 print(df['LABEL'].value_counts())
 
-# sys.exit()
-
 print('Separando os dados em features (X) e labels (y)')
 X = df['TEXTO']
 y = df['LABEL']
 
 print('Dividindo os dados em conjunto de treinamento e conjunto de teste')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# print('Definindo o vocab_size')
-# print('Extraindo os textos da coluna TEXTO')
-# textos = df['TEXTO'].tolist()
-# print('Criando uma instância do CountVectorizer')
-# vectorizer = CountVectorizer()
-# print('Realizar o fit_transform nos textos')
-# X_textos = vectorizer.fit_transform(textos)
-# print('Obtendo o vocabulário do CountVectorizer')
-# vocabulario = vectorizer.vocabulary_
-# print('Obtendo as palavras únicas do vocabulário')
-# palavras_unicas = list(vocabulario.keys())
-# print('Calculando o vocab_size')
-# vocab_size = len(palavras_unicas)
-
-# codigo acima executado uma vez, colocando agora o valor fixo resultante:
-# vocab_size = 2056447
-# print('=> vocab_size:', vocab_size)
-
-# print('Calculando o comprimento de cada sequência de texto')
-# comprimentos = [len(texto.split()) for texto in textos]
-# print('Calculando o comprimento máximo das sequências')
-# max_length = max(comprimentos)
-
-# codigo acima executado uma vez, colocando agora o valor fixo resultante:
-# max_length = 1575215
-# print("=> max_length:", max_length)
-
-# print('Contando número de classes')
-# num_classes = df['LABEL'].nunique()
-# print("=> num_classes:", num_classes)
-
-# print('Definindo a arquitetura da rede neural')
-# embedding_dim = 128
-# model = Sequential([
-#     Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_length),
-#     LSTM(units=100),
-#     Dense(units=num_classes, activation='softmax')
-# ])
 
 print('Tokenização e padding dos textos')
 max_words = 10000
@@ -115,16 +70,9 @@ model = Sequential([
 print('Compilando o modelo')
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# print('Criando um pipeline que transforma os textos em vetores e treina o modelo Naive Bayes')
-# model = make_pipeline(CountVectorizer(), model)
 
 print('Treinando o modelo')
-# model.fit(X_train, y_train)
-# Treinar modelo
 model.fit(X_train_padded, y_train, epochs=10, batch_size=64, validation_data=(X_test_padded, y_test))
-
-# print('Fazendo previsões no conjunto de teste')
-# y_pred = model.predict(X_test)
 
 print('Calculando a precisão do modelo')
 # accuracy = accuracy_score(y_test, y_pred)
@@ -132,7 +80,6 @@ loss, accuracy = model.evaluate(X_test_padded, y_test)
 print(f'Acurácia do modelo: {accuracy}')
 
 print('Salvando o modelo treinado')
-# dump(model, 'modelo_classificacao.pkl')
 model.save('modelo_lstm.keras')
 
 # resultado 0.6
